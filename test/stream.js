f41f3cf5068495a124ab72f604390d346456a2ee
Jean-Charles Sisk <jasisk@gmail.com>
2015-03-11 00:03:53 -0400

    tests to properly stream out octet-streams

---
+++ test/stream.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var url = require('url');
+var test = require('tape');
+var Hapi = require('hapi');
+var nock = require('nock');
+var settings = require('../config/defaults');
+var kappa = require('../');
+var Stream = require('stream');
+var Wreck = require('wreck');
+
+test('proxy octet-streams', function (t) {
+    var uri, server, stream;
+
+    // Configure nock for this single request.
+    uri = url.parse(settings.paths[0]);
+
+    nock(uri.protocol + '//' + uri.host).get(uri.pathname + 'cdb').reply(200, function () {
+      stream = new Stream.PassThrough();
+      stream.push('pre'); // doing this to flush the headers early
+      return stream;
+    }, {'content-type': 'application/octet-stream'});
+
+    t.plan(6);
+
+    server = new Hapi.Server(0);
+    server.pack.register({
+        plugin: kappa,
+        options: settings
+    }, function (err) {
+        t.error(err);
+
+        server.start(function () {
+            Wreck.request('get', 'http://localhost:' + server.info.port + '/cdb', null, function (err, res) {
+                t.error(err);
+                t.strictEqual(res.statusCode, 200);
+                Wreck.toReadableStream('post').pipe(stream);
+                Wreck.read(res, null, function (err, payload) {
+                  t.error(err);
+                  t.ok(payload);
+                  t.equal(payload.toString(), 'prepost');
+                  server.stop();
+                });
+            });
+        });
+    });
+});
